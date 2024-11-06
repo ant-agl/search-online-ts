f211d3243bbf524706b92de661a85a8f32224549
ant-agl <aglanton31@gmail.com>
2024-11-07 01:30:55 +0300

    parallax

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from "vue";
 import HeaderMenu from "@/components/HeaderMenu.vue";
 import ConnectUs from "@/pagesComponents/ConnectUs.vue";
 import FooterSearchOnline from "@/pagesComponents/FooterSearchOnline.vue";
@@ -8,18 +9,50 @@ import PopularCards from "@/pagesComponents/PopularCards.vue";
 import StagesCard from "@/pagesComponents/StagesCard.vue";
 import SwiperCategory from "@/pagesComponents/SwiperCategory.vue";
 import SwiperClients from "@/pagesComponents/SwiperClients.vue";
+
+let lastScrollY = window.scrollY;
+const parallaxFactor = 0.5;
+const bgEllipses = ref();
+
+function updateParallax() {
+  const scrollPosition = window.scrollY;
+
+  if (bgEllipses.value && Math.abs(scrollPosition - lastScrollY) > 1) {
+    bgEllipses.value.style.transform = `translateY(${
+      scrollPosition * parallaxFactor
+    }px)`;
+    lastScrollY = scrollPosition;
+  }
+
+  requestAnimationFrame(updateParallax);
+}
+requestAnimationFrame(updateParallax);
 </script>
 <template>
   <div class="main-page">
     <HeaderMenu />
     <HomeComponent />
 
-    <SwiperCategory />
-    <StagesCard />
+    <div class="main-section">
+      <SwiperCategory />
+      <StagesCard />
 
-    <PopularCards />
-    <SwiperClients />
-    <ConnectUs />
+      <PopularCards />
+      <SwiperClients />
+      <ConnectUs />
+
+      <div class="bg-ellipses" ref="bgEllipses">
+        <img src="@/img/main/ellipse.png" class="bg-ellipses__item" />
+        <img src="@/img/main/ellipse.png" class="bg-ellipses__item" />
+        <img src="@/img/main/ellipse.png" class="bg-ellipses__item" />
+        <img src="@/img/main/ellipse.png" class="bg-ellipses__item" />
+        <img src="@/img/main/ellipse.png" class="bg-ellipses__item" />
+        <img src="@/img/main/ellipse.png" class="bg-ellipses__item" />
+        <img src="@/img/main/ellipse.png" class="bg-ellipses__item" />
+        <img src="@/img/main/ellipse.png" class="bg-ellipses__item" />
+      </div>
+    </div>
+
     <FooterSearchOnline />
   </div>
 </template>
@@ -48,5 +81,59 @@ a {
   .padding-section {
     padding: 40px 0;
   }
+}
+
+.main-section {
+  position: relative;
+  overflow: hidden;
+}
+.bg-ellipses {
+  position: absolute;
+  inset: 0;
+  width: 100%;
+  pointer-events: none;
+}
+.bg-ellipses__item {
+  position: absolute;
+}
+.bg-ellipses__item:nth-child(1) {
+  width: 170px;
+  left: -60px;
+  top: 200px;
+}
+.bg-ellipses__item:nth-child(2) {
+  width: 100px;
+  right: -60px;
+  top: 180px;
+}
+.bg-ellipses__item:nth-child(3) {
+  width: 300px;
+  right: -60px;
+  top: 600px;
+}
+.bg-ellipses__item:nth-child(4) {
+  width: 150px;
+  left: -40px;
+  top: 700px;
+}
+.bg-ellipses__item:nth-child(5) {
+  width: 200px;
+  left: 10px;
+  top: 1250px;
+}
+.bg-ellipses__item:nth-child(6) {
+  width: 140px;
+  right: 60px;
+  top: 1300px;
+}
+.bg-ellipses__item:nth-child(7) {
+  width: 110px;
+  left: 70px;
+  top: 1800px;
+}
+.bg-ellipses__item:nth-child(8) {
+  width: 150px;
+  right: 48px;
+  top: 1900px;
 }
 </style>
